58c07131b2d0f5d734bfe38ad7c4cbc22826fc8b
coxcore <ascript0@gmail.com>
2020-11-27 20:03:09 +0900

    add prototype

--- README.md
+++ README.md
@@ -1,30 +1,85 @@
-# react-vac
+# react-vac (prototype)
 
-> View Asset Component(VAC)
+> This component is a debugging tool that helps you develop components without View(JSX). It provides an optimal solution for [VAC Pattern](./docs/VAC_PATTERN.md) type development.
+
+![vac pattern](./docs/assets/img/vac_pattern_s1.png?raw=true)
 
 [![NPM](https://img.shields.io/npm/v/react-vac.svg)](https://www.npmjs.com/package/react-vac) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
 
 ## Install
 
 ```bash
-npm install --save react-vac
+npm install react-vac --save-dev
+```
+
+or
+
+```bash
+yarn add react-vac --dev
 ```
 
 ## Usage
 
+![basic usage](./docs/assets/img/example_vac_basic_s1.png?raw=true)
+
 ```jsx
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import VAC from "react-vac";
 
-import MyComponent from 'react-vac'
-import 'react-vac/dist/index.css'
+const Component = () => {
+  const works = () => {
+    console.log("do something");
+  };
 
-class Example extends Component {
-  render() {
-    return <MyComponent />
-  }
-}
+  // props of component to be developed
+  const viewComponentProps = {
+    // properties
+    value: "test value",
+    someValue: { foo: "var" },
+
+    // callbacks
+    onClick: (event) => console.log("click!"),
+    onSomeEvent: (event) => works(),
+  };
+
+  // view debugging
+  return (
+    <VAC name="Dummy View" data={viewComponentProps} />
+    // <ViewComponent {...viewComponentProps} />
+  );
+};
+
+export default Component;
 ```
+
+[Learn More>](./docs/VAC_USAGE.md)
+
+## Examples
+
+![basic example](./docs/assets/img/example_vac_spinbox_s1.png?raw=true)
+
+```jsx
+import React, { useState } from "react";
+import VAC from "react-vac";
+
+const SpinBoxUI = () => {
+  const [value, setValue] = useState(0);
+
+  const spinBoxProps = {
+    // properties
+    value,
+
+    // callbacks
+    onIncrease: () => setValue(value + 1),
+    onDecrease: () => setValue(value - 1),
+  };
+
+  return <VAC name="SpinBox" data={spinBoxProps} />;
+};
+```
+
+[Show More>](./docs/VAC_EXAMPLES.md)
 
 ## License
 
-MIT © [coxcore](https://github.com/coxcore)
+MIT © [Park U-yeong](https://github.com/coxcore)
--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,97 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-import { ExampleComponent } from 'react-vac'
-import 'react-vac/dist/index.css'
+// import VAC from 'react-vac'
+import VAC, { VACList, VACInput } from 'react-vac';
+
+const Component = () => {
+    const works = () => {
+        console.log('do something');
+    };
+
+    // props of component to be developed
+    const viewComponentProps = {
+        // properties
+        value: 'test value',
+        someValue: { foo: 'var' },
+
+        // callbacks
+        onClick: (event) => console.log('click!'),
+        onSomeEvent: (event) => works(),
+    };
+
+    // view debugging
+    return (
+        <VAC name="Dummy View" data={viewComponentProps} />
+        // <ViewComponent {...viewComponentProps} />
+    );
+};
+
+const List = () => {
+    const model = [
+        { id: 1606453217589, value: 'Todo 1', checked: false },
+        { id: 1606453217589, value: 'Todo 1', checked: false },
+        { id: 1606453217589, value: 'Todo 1', checked: false },
+    ];
+
+    const each = ({ id, value, checked }, index) => ({
+        key: id,
+        id,
+        value,
+        checked,
+        onCheck: () => console.log(id),
+        onDelete: () => console.log(id),
+    });
+
+    const listComponentProps = {
+        list: model,
+        each,
+    };
+
+    return <VACList name="Dummy List" data={listComponentProps} />;
+};
+
+const Input = () => {
+    const [value, setValue] = useState('test');
+
+    const inputComponentProps = {
+        id: 'testid',
+        value,
+
+        onChange: (event) => {
+            const value = event.currentTarget.value;
+            console.log(value);
+            setValue(value);
+        },
+        onSubmit: (event) => console.log('submit :', value),
+    };
+
+    return <VACInput name="Dummy Input" data={inputComponentProps} />;
+};
+
+const SpinBox = () => {
+    const [value, setValue] = useState(0);
+
+    const inputComponentProps = {
+        // properties
+        value,
+
+        // callbacks
+        onIncrease: () => setValue(value + 1),
+        onDecrease: () => setValue(value - 1),
+    };
+
+    return <VAC name="SpinBox" data={inputComponentProps} />;
+};
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
-}
+    return (
+        <div style={{ width: '700px', padding: '1px' }}>
+            <Component />
+            <List />
+            <Input />
+            <SpinBox />
+        </div>
+    );
+};
 
-export default App
+export default App;
